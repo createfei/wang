15558287ecc313f7c777ecace33ba145ea42505d
createfei <2015215178@qq.com>
2025-08-16 16:27:04 +0800

    22

---
+++ config.h
@@ -0,0 +1,15 @@
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+// 按键GPIO定义
+#define KEY1_PIN      GPIO_Pin_0
+#define KEY2_PIN      GPIO_Pin_1
+#define KEY3_PIN      GPIO_Pin_2
+#define KEY4_PIN      GPIO_Pin_3
+#define KEY_GPIO_PORT GPIOA
+
+// 串口定义
+#define BT_USART      USART1
+#define BT_BAUDRATE   9600
+
+#endif
